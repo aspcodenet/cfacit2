4b6bd62b9372ee708945428085c7234fb347b702
Stefan <stefan.holmberg@systementor.se>
2023-02-03 13:00:50 +0100

    a

--- main.c
+++ main.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 int main(){
 
-    int tal;
-    printf("Mata in ett tal");
-    scanf(" %d", &tal);
-    for(int i = tal-1; i >0; i-- )
-        printf("%d\n",i);
+    srand(time(NULL));
+    while(1){
+        int dice1 = rand() % 6 + 1;
+        int dice2 =  rand() % 6 + 1;
+
+        printf("Rolling the dices...\n");
+        printf("The values are...\n");
+        printf("%d\n%d\n",dice1,dice2);
+
+        printf("Roll the dices again?");
+        char ch;
+        scanf(" %c", &ch);
+        if(ch != 'y' )
+            break;
+    }
 
 }
