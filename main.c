a96bcc9488736d3aa607c1cd090b7815c50ab780
Stefan <stefan.holmberg@systementor.se>
2023-02-03 12:50:40 +0100

    4

--- main.c
+++ main.c
@@ -4,99 +4,14 @@
 
 int main(){
 
-
-    while(1){
-        int tal1;
-        int tal2;
-        printf("Tal 1:");
-        scanf(" %d", &tal1);    
-        printf("Tal 2:");
-        scanf(" %d", &tal2);    
-
-        // int summa = tal1 + tal2;
-        // printf("Summan blev %d\n", summa);
-
-        printf("Summan blev %d\n",tal1 + tal2); 
-
-        printf("Viil du fortsätta(1=Ja, 0=Nej)?\n"); 
-        int shouldContinue;
-        scanf(" %d", &shouldContinue);    
-        if(shouldContinue == 0)
-            break;
+    int summa  = 0;
+    for(int i = 0; i < 10; i++){
+        int tal;
+        printf("Tal %d", i+1);
+        scanf(" %d", &tal);
+        summa += tal;
     }
-    
-
-
-
-
-
-
-
-
-
-
-
-
-    // if(start < slut) {
-    //     for(int i=start; i <= slut ; i++ ){
-    //         printf("%d\n",i);
-    //     }
-
-    //     int i=start;
-    //     while(i <= slut){
-    //         printf("%d\n",i);
-    //         i++;
-    //     }
-    // }else{ //slut >= start
-    //     for(int i=start; i >= slut ; i-- ){
-    //         printf("%d\n",i);
-    //     }
-
-    //     int i=start;
-    //     while(i >= slut){
-    //         printf("%d\n",i);
-    //         i--;
-    //     }
-
-    // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // if(start < slut){
-    //     for(int i=start; i <= slut ; i++ ){
-    //         printf("%d\n",i);
-    //     }
-
-    //     int i=start;
-    //     while(i <= slut){
-    //         printf("%d\n",i);
-    //         i++;
-    //     }
-    // }else{
-    //     for(int i=start; i >= slut ; i-- ){
-    //         printf("%d\n",i);
-    //     }
-
-    //     int i=start;
-    //     while(i >= slut){
-    //         printf("%d\n",i);
-    //         i--;
-    //     }
-
-    // }
-
+    printf("Summa %d", summa);
 
 
 }
